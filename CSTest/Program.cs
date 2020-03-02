7be672de8a67e1f69488808e927fdc9a8ed3a14e
jing.hua@live.com <Jinghua851123>
2020-03-02 21:17:12 +0800

    添加项目文件。

---
+++ CSTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("xxx");
+            Console.Write("xxx2");
+        }
+    }
+}
---
+++ CSTest/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSTest
+{
+    class Stack
+    {
+        Entry top;
+        public void Push(object data)
+        {
+            top = new Entry(top, data);
+        }
+        public object Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+
+        // static void Main() { Stack s = new Stack(); s.Push(1); s.Push(10); s.Push(100); Console.WriteLine(s.Pop()); Console.WriteLine(s.Pop()); Console.WriteLine(s.Pop()); }
+
+        class Entry
+        {
+            public Entry next;
+            public object data;
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+}
+
